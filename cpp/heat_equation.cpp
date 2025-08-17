1a77b324ec9239531affb35a37d786e84ab07b9c
kacper16621 <kacper.klos@proton.me>
2025-08-17 19:24:42 +0200

    Documentation for functions

--- cpp/heat_equation.cpp
+++ cpp/heat_equation.cpp
@@ -8,6 +8,13 @@
 
 std::vector<data_type> time_step(const std::vector<data_type> &temperature,
                                  const data_type diffusion_const) {
+    /**
+     * Performs one step of 1D heat diffusion equation.
+     *
+     * @param temperature Previous state of simulation.
+     * @param diffusion_const Constant determining how strong neightbour affects each other.
+     * @return Next state of temperature.
+     */
 
     // Creating new temperature vector.
     std::vector<data_type> new_temperature(temperature.size());
@@ -23,6 +30,15 @@ std::vector<data_type> time_step(const std::vector<data_type> &temperature,
 std::vector<std::vector<data_type>> simulate(const std::vector<data_type> &initial_conditions,
                                              const data_type diffusion_constant,
                                              const uint32_t frames_count) {
+    /**
+     * Performs full simulation for multiple frames.
+     *
+     * @param initial_conditions Initial state of simulation.
+     * @param diffusion_const Constant determining how strong neightbour affects each other.
+     * @param frames_count Number of frames in simulation.
+     * @return Vector with frames of a simulation
+     */
+
     // Create vector for frames
     std::vector<std::vector<data_type>> frames(frames_count+1);
     frames[0] = initial_conditions;
@@ -39,6 +55,19 @@ std::vector<std::vector<data_type>> simulation_init(const std::vector<data_type>
                                                     const data_type dt,
                                                     const data_type dx,
                                                     const data_type a) {
+    /**
+     * Calculate the constants, check input and launch simulation.
+     *
+     * @param initial_conditions Initial state of simulation.
+     * @param time Full times the simulation lasts.
+     * @param dt Time of one frame.
+     * @param dx Distance between the nodes.
+     * @param a Constant defining speed of heat transmition in material.
+     * @return Vector with frames of a simulation
+     *
+     * @throws std::invalid_argument if full time of simulation is smaller then step or inital conditions are smaller then 3.
+     */
+
     // Analize input
     if (time < dt) {
         throw std::invalid_argument("Time must be greater or equal to dt");
