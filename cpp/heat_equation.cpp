414fdc983e2da90ccd53f142ef11a58fd0cd596b
kacper16621 <kacper.klos@proton.me>
2025-08-17 18:59:18 +0200

    Function for simulation

--- cpp/heat_equation.cpp
+++ cpp/heat_equation.cpp
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <vector>
+#include <cstdint>
 
 #define data_type float
 
-std::vector<data_type> time_setp(const std::vector<data_type> &temperature,
+std::vector<data_type> time_step(const std::vector<data_type> &temperature,
                                  const data_type diffusion_const) {
 
     // Creating new temperature vector.
@@ -15,4 +16,18 @@ std::vector<data_type> time_setp(const std::vector<data_type> &temperature,
     }
 
     return new_temperature;
+}
+
+std::vector<std::vector<data_type>> simulate(const std::vector<data_type> &initial_conditions,
+                                             const data_type diffusion_constant,
+                                             const uint32_t frames_count) {
+    // Create vector for frames
+    std::vector<std::vector<data_type>> frames(frames_count+1);
+    frames[0] = initial_conditions;
+
+    // Fill frames with simulation predictions
+    for (int i = 1; i < frames_count; ++i) {
+        frames[i] = time_step(frames[i-1], diffusion_constant);
+    }
+    return frames;
 }
